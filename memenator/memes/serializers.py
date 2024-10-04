4f007b5e2104b857f0234decd1b07810de7db33f
Kravchenko Georgy <88142456+Kravchenko228@users.noreply.github.com>
2024-10-04 11:56:15 +0000

    top_text or bottom_text uses the template's default values if not provided

--- memenator/memes/serializers.py
+++ memenator/memes/serializers.py
@@ -16,24 +16,49 @@ class MemeSerializer(serializers.ModelSerializer):
         fields = ['id', 'template', 'top_text', 'bottom_text', 'created_by', 'creation_date']
 
 class MemeCreateSerializer(serializers.ModelSerializer):
+    top_text = serializers.CharField(allow_blank=True, required=False)
+    bottom_text = serializers.CharField(allow_blank=True, required=False)
+
     class Meta:
         model = Meme
         fields = ['template', 'top_text', 'bottom_text']
 
+    def validate(self, data):
+        """
+        Custom validation to allow blank values and replace them with the default template text.
+        """
+        template = data.get('template')
+
+        # Use default text from template if top_text or bottom_text is blank or None
+        if not data.get('top_text'):
+            data['top_text'] = template.default_top_text
+        if not data.get('bottom_text'):
+            data['bottom_text'] = template.default_bottom_text
+
+        return data
+
     def create(self, validated_data):
         request = self.context.get('request')
-        template = validated_data.get('template')
-
-        top_text = validated_data.get('top_text', template.default_top_text)
-        bottom_text = validated_data.get('bottom_text', template.default_bottom_text)
-
         meme = Meme.objects.create(
-            created_by=request.user, 
-            template=template, 
-            top_text=top_text, 
-            bottom_text=bottom_text
+            created_by=request.user,
+            template=validated_data.get('template'),
+            top_text=validated_data.get('top_text'),
+            bottom_text=validated_data.get('bottom_text')
         )
         return meme
+
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        meme = Meme.objects.create(
+            created_by=request.user,
+            template=validated_data.get('template'),
+            top_text=validated_data.get('top_text'),
+            bottom_text=validated_data.get('bottom_text')
+        )
+        return meme
+
+
 
 
 class RatingSerializer(serializers.ModelSerializer):
